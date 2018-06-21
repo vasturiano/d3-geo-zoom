60552c37dd2b9985a2118180189ea6882a4fc269
vasco <vastur@gmail.com>
2018-06-21 00:02:04 -0700

    Export default module

--- rollup.config.module.js
+++ rollup.config.module.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import { name, dependencies } from './package.json';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/geoZoom.js',
   output: [
     {
       format: 'cjs',
