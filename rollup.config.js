a05624d89d42d9e27fa30b64a63954f1b4d31766
vasco <vastur@gmail.com>
2020-10-12 23:32:20 -0700

    Bump deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,54 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      extend: true,
-      name: 'd3',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonJs(),
-    babel({ exclude: 'node_modules/**' })
-  ]
+const umdConf = {
+  format: 'umd',
+  name: 'd3',
+  extend: true,
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  { // UMD
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonJs()
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/geoZoom.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  }
+];
